55ede5605afb4ac903167599badcf01b9838471b
Heath Manning <heath@manningf.com>
2018-11-22 20:08:27 +1100

    Added left click by checking if mouse over the params. refactored HTML.

--- classes.js
+++ classes.js
@@ -17,13 +17,13 @@ function Orbiter (initPos, initVel, radius) {
 
     this.Update = function (deltaTime) {
 
-        if(this.active) {
+        if(this.active || true) {
             if (deltaTime == null) {
                 deltaTime = fixedDeltaTime*1;
             }
 
             var earthDist = p5.Vector.dist(this.pos, earthPos)
-            var accMag = ((G*earthMass) / (earthDist*earthDist)) * timeScale;
+            var accMag = ((G*earthMass) / (earthDist*earthDist)) * deltaTime;
 
             this.vel.add(p5.Vector.sub(earthPos, this.pos).normalize().mult(accMag));
 
@@ -38,7 +38,7 @@ function Orbiter (initPos, initVel, radius) {
 
 
         let dist = p5.Vector.dist(this.pos, earthPos);
-        let radii = this.rad + earthMass/6;
+        let radii = this.rad + earthRadius;
         //let earthRad = earthMass/8;
         if (Math.abs(dist) < radii) {
             this.delete = true;
@@ -66,35 +66,37 @@ function Orbiter (initPos, initVel, radius) {
 
             for (var i = 0; i < orbiters.length; i++) {
                 if (orbiters[i] != this) {
-                    let dist = p5.Vector.dist(this.pos, orbiters[i].pos);
-                    let radii = this.rad + orbiters[i].rad;
-                    if (Math.abs(dist) < radii) {
-                        print("collision");
+                    if ((this.pos.x + this.rad + orbiters[i].rad > orbiters[i].pos.x && this.pos.x < orbiters[i].pos.x + this.rad + orbiters[i].rad && this.pos.y + this.rad + orbiters[i].rad > orbiters[i].pos.y && this.pos.y < orbiters[i].pos.y + this.rad + orbiters[i].rad)) {
+                        let dist = p5.Vector.dist(this.pos, orbiters[i].pos);
+                        let radii = this.rad + orbiters[i].rad;
+                        if (Math.abs(dist) < radii) {
+                            print("collision");
 
-                        let dir = p5.Vector.sub(orbiters[i].pos, this.pos).normalize();
-                        this.pos = p5.Vector.mult(orbiters[i].pos, 1);
-                        this.pos.sub(p5.Vector.mult(dir, radii));
+                            let dir = p5.Vector.sub(orbiters[i].pos, this.pos).normalize();
+                            this.pos = p5.Vector.mult(orbiters[i].pos, 1);
+                            this.pos.sub(p5.Vector.mult(dir, radii));
 
-                        let colPoint = p5.Vector.mult(dir, this.rad).add(this.pos);
+                            let colPoint = p5.Vector.mult(dir, this.rad).add(this.pos);
 
-                        let VelX1 = (this.vel.x * (this.mass - orbiters[i].mass) + (2 * orbiters[i].mass * orbiters[i].vel.x)) / (this.mass + orbiters[i].mass);
-                        let VelY1 = (this.vel.y * (this.mass - orbiters[i].mass) + (2 * orbiters[i].mass * orbiters[i].vel.y)) / (this.mass + orbiters[i].mass);
-                        let VelX2 = (orbiters[i].vel.x * (orbiters[i].mass - this.mass) + (2 * this.mass * this.vel.x)) / (orbiters[i].mass + this.mass);
-                        let VelY2 = (orbiters[i].vel.y * (orbiters[i].mass - this.mass) + (2 * this.mass * this.vel.y)) / (orbiters[i].mass + this.mass);
+                            let VelX1 = (this.vel.x * (this.mass - orbiters[i].mass) + (2 * orbiters[i].mass * orbiters[i].vel.x)) / (this.mass + orbiters[i].mass);
+                            let VelY1 = (this.vel.y * (this.mass - orbiters[i].mass) + (2 * orbiters[i].mass * orbiters[i].vel.y)) / (this.mass + orbiters[i].mass);
+                            let VelX2 = (orbiters[i].vel.x * (orbiters[i].mass - this.mass) + (2 * this.mass * this.vel.x)) / (orbiters[i].mass + this.mass);
+                            let VelY2 = (orbiters[i].vel.y * (orbiters[i].mass - this.mass) + (2 * this.mass * this.vel.y)) / (orbiters[i].mass + this.mass);
 
-                        this.vel = createVector(VelX1, VelY1).mult(collisionDrag);
-                        orbiters[i].vel = createVector(VelX2, VelY2).mult(collisionDrag);
+                            this.vel = createVector(VelX1, VelY1).mult(collisionDrag);
+                            orbiters[i].vel = createVector(VelX2, VelY2).mult(collisionDrag);
 
-                        this.pos.add(p5.Vector.mult(this.vel, 1).normalize());
-                        orbiters[i].pos.add(p5.Vector.mult(orbiters[i].vel, 1).normalize());
+                            this.pos.add(p5.Vector.mult(this.vel, 1).normalize());
+                            orbiters[i].pos.add(p5.Vector.mult(orbiters[i].vel, 1).normalize());
 
-                        //this.Update();
-                        //orbiters[i].Update();
+                            //this.Update();
+                            //orbiters[i].Update();
 
-                        if(this.rad >= 2 && Math.round(random(Math.round((this.rad - orbiters[i].rad) / this.rad))) == 0) {
+                            if(this.rad >= 2 && Math.round(random(Math.round((this.rad - orbiters[i].rad) / this.rad))) == 0) {
 
-                            this.Fragment();
+                                this.Fragment();
 
+                            }
                         }
                     }
                 }
@@ -105,8 +107,10 @@ function Orbiter (initPos, initVel, radius) {
     this.Draw = function () {
         push();
         if(this.delete) {
-            fill(255, 195, 195)
+            fill(255, 195, 195, 31);
             this.rad = this.rad * 3;
+            ellipse(this.pos.x, this.pos.y, this.rad*4);
+            fill(255, 195, 195, 255);
         } else {
             fill(215);
         }
@@ -118,8 +122,8 @@ function Orbiter (initPos, initVel, radius) {
 
     this.Fragment = function () {
 
-        nextOrbiters.push(new Orbiter(p5.Vector.add(this.pos, createVector(-this.rad/2, -this.rad/2)), p5.Vector.mult(this.vel, 1), this.rad/2));
-        nextOrbiters.push(new Orbiter(p5.Vector.add(this.pos, createVector(this.rad/2, this.rad/2)), p5.Vector.mult(this.vel, 1), this.rad/2));
+        nextOrbiters.push(new Orbiter(p5.Vector.add(this.pos, createVector(-this.rad/2-1, -this.rad/2-1)), p5.Vector.mult(this.vel, 1), this.rad/2));
+        nextOrbiters.push(new Orbiter(p5.Vector.add(this.pos, createVector(this.rad/2+1, this.rad/2+1)), p5.Vector.mult(this.vel, 1), this.rad/2));
 
         for( var i = 0; i < orbiters.length; i++){
             if ( orbiters[i] == this) {
--- sketch.js
+++ sketch.js
@@ -6,38 +6,42 @@
 var fixedDeltaTime = 0, tempDeltaTime = 0;
 var frameTime = 0;
 
-var timeScale = 6;
+var timeScale = 8;
 
 var G = 32;
 
 var trail = 0;
 
 var earthPos = null;
-var earthMass = 512;
+var earthMass = 2048;
+var earthRadius = 64;
 var orbiters = [], nextOrbiters = [];
 
 var collisionDrag = 1;
-var collisions = false;
+var collisions = true;
 
 var GSlider, massSlider, timeSlider, trailSlider;
 
-var mousePressedPos = null, mouseReleasedPos = null, mousePos = null, cCheckBox = null;
+var mousePressedPos = null, mouseReleasedPos = null, mousePos = null;
 
 var lastOrbit;
+
+var isOverParams = false, paramDiv = null;
 
 function reset() {
 
     orbiters = [];
     nextOrbiters = [];
 
-    GSlider.value(32);
-    massSlider.value(512);
-    timeSlider.value(6);
-    trailSlider.value(127);
-    trail = trailSlider.value();
-    G = GSlider.value();
-    earthMass = massSlider.value();
-    timeScale = timeSlider.value();
+    GSlider.value = 32;
+    massSlider.value = 512;
+    timeSlider.value = 16;
+    trailSlider.value = 127;
+
+    trail = trailSlider.value;
+    G = GSlider.value;
+    earthMass = massSlider.value;
+    timeScale = timeSlider.value;
 
 }
 
@@ -46,28 +50,21 @@ function setup() {
     print("setup function called");
     //frameRate(30);
 
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight).elt.style.zIndex="-5";
     fill(215);
     strokeJoin(ROUND);
 	rectMode(RADIUS);
 	ellipseMode(RADIUS);
     noStroke();
 
-    GSlider = createSlider(0.1, 100, 32);
-    GSlider.position(24, 24);
-    massSlider = createSlider(0.1, 4096, 512);
-    massSlider.position(24, 48);
-    timeSlider = createSlider(-40, 32, 6);
-    timeSlider.position(24, 72);
-    trailSlider = createSlider(1, 255, 127, 1);
-    trailSlider.position(24, 96);
+    paramDiv = document.getElementById("Parameters");
 
-    trail = trailSlider.value();
-    G = GSlider.value();
-    earthMass = massSlider.value();
-    timeScale = timeSlider.value();
+    GSlider = document.getElementById("G");
+    massSlider = document.getElementById("Mass");
+    timeSlider = document.getElementById("Time");
+    trailSlider = document.getElementById("Trail");
 
-    cCheckBox = document.getElementById("collisions");
+    reset();
 
     orbiters = [];
     earthPos = createVector(width/2, height/2);
@@ -85,22 +82,18 @@ function draw() {
 
     mousePos = createVector(mouseX*1, mouseY*1);
 
-    G = GSlider.value();
-    earthMass = massSlider.value();
-    timeScale = timeSlider.value();
-    collisions = cCheckBox.checked;
-    trail = trailSlider.value();
+    G = GSlider.value;
+    earthMass = massSlider.value;
+    timeScale = timeSlider.value;
+    trail = trailSlider.value;
+
+
 
     fixedDeltaTime = ((Date.now()/1000 - frameTime) * timeScale);
     //print(fixedDeltaTime);
     frameTime = Date.now()/1000;
 
     //background (47);
-    if (mouseIsPressed && mouseButton === RIGHT) {
-        //background(47);
-
-        predictOrbit(new Orbiter(p5.Vector.mult(mousePressedPos, 1), p5.Vector.sub(mousePos, mousePressedPos), 8));
-    }
     push();
     fill(47, trail);
     rect(earthPos.x, earthPos.y, width/2, height/2);
@@ -119,35 +112,46 @@ function draw() {
     }
 
 
+
     push();
     fill(63);
-    ellipse(earthPos.x, earthPos.y, earthMass/6);
+    ellipse(earthPos.x, earthPos.y, earthRadius*8/6);
     fill(215);
-    ellipse(earthPos.x, earthPos.y, earthMass/8);
+    ellipse(earthPos.x, earthPos.y, earthRadius);
     pop();
+
+
 
     for (var i = 0; i < orbiters.length; i++) {
         orbiters[i].Draw();
     }
+
+    if (mouseIsPressed && isOverParams == false) {
+        //background(47);
+
+        predictOrbit();
+    }
+
+    if (Date.now()/1000 - frameTime >= 3) {
+        noLoop();
+    }
 }
 
-function predictOrbit(obj) {
+function predictOrbit() {
+    let obj = new Orbiter(p5.Vector.mult(mousePressedPos, 1), p5.Vector.sub(mousePos, mousePressedPos).mult(tempDeltaTime), 8);
+
     print("tracing orbit");
 
     push();
     noFill();
-    strokeWeight(2);
-    stroke(215, 143, 143);
-
-    //print(mousePressedPos);
-    //print(mousePos);
-    push();
+    stroke(215, 143, 143, 255);
     strokeWeight(4);
     line(mousePressedPos.x, mousePressedPos.y, mousePos.x, mousePos.y);
     ellipse(mousePos.x, mousePos.y, 8, 8);
-    pop();
 
-    let iters = Math.round((p5.Vector.dist(mousePressedPos, earthPos) * obj.initVel.mag()) / (Math.abs(timeScale*2)));
+    let iters = Math.round((16/tempDeltaTime) * obj.initVel.magSq());
+
+    print(iters);
 
     for (i = 0; i < iters; i++) {
         let pos = p5.Vector.mult(obj.pos, 1);
@@ -159,18 +163,33 @@ function predictOrbit(obj) {
 
 function mousePressed() {
     print("mouse pressed");
+
     mousePressedPos = createVector(mouseX*1, mouseY*1);
     tempDeltaTime = fixedDeltaTime*1;
+
+    let divDim = [parseInt(paramDiv.style.left,10), parseInt(paramDiv.style.top,10), parseInt(paramDiv.style.width,10), parseInt(paramDiv.style.height,10)];
+
+    if (mousePressedPos.x >= divDim[0] && mousePressedPos.x <= divDim[0] + divDim[2] && mousePressedPos.y >= divDim[1] && mousePressedPos.y <= divDim[1] + divDim[3]) {
+        isOverParams = true;
+    }
+
+    if(isOverParams) {
+        return false;
+    }
+
     //return false;
 }
 
 function mouseReleased() {
     print("mouse released");
     mouseReleasedPos = createVector(mouseX*1, mouseY*1);
-    if(mouseButton === RIGHT) {
-        lastOrbit = new Orbiter(mousePressedPos, p5.Vector.sub(mousePos, mousePressedPos), 16);
-        nextOrbiters.push(lastOrbit.Clone());
+    if(isOverParams) {
+        isOverParams = false;
+        return false;
     }
+    lastOrbit = new Orbiter(mousePressedPos, p5.Vector.sub(mousePos, mousePressedPos).mult(tempDeltaTime), 8);
+    nextOrbiters.push(lastOrbit.Clone());
+
     //return false;
 }
 
